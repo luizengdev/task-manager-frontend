caa2d9cd74a43fbe10fcb76bc56b4b47019090d6
luizengdev <luizengdev@gmail.com>
2024-01-22 16:03:01 -0300

    feat: add react-alert setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 import "./index.scss";
 import App from "./App.jsx";
 
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 2500,
+  offset: "30px",
+  transition: transitions.SCALE,
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>
 );

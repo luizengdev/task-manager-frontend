30f16b3803bcdb041a37b77bd1de106cb5887dfe
luizengdev <luizengdev@gmail.com>
2024-01-26 21:14:33 -0300

    feat: add navigation to sign out buttom in Sidebar componet

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 import CustomButton from "./CustomButton";
 
 import "./Sidebar.scss";
 import logo from "../assets/logo.svg";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleSignOutClick = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="sidebar-container">
       <div className="logo">
@@ -11,7 +19,7 @@ const Sidebar = () => {
       </div>
 
       <div className="sign-out">
-        <CustomButton>Sair</CustomButton>
+        <CustomButton onClick={handleSignOutClick}>Sair</CustomButton>
       </div>
     </div>
   );

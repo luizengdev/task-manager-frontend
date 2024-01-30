75c3d766f32046b570e86b94112ec16863533974
luizengdev <luizengdev@gmail.com>
2024-01-29 21:17:33 -0300

    refactor: add vercel

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import "./App.scss";
 const App = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
